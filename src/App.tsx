10e1080f6a1002f9bda9d3d8c70bfb55a4bc8289
Tiago Silva <tiagopereirasilva5425@gamil.com>
2025-05-17 18:10:32 -0300

    configurações das letras usadas

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,55 @@ import { Letter } from './components/Letter'
 
 import { Input } from './components/Input'
 import { Button } from './components/Button'
-import { LettersUsed } from './components/LettersUsed'
+import { LettersUsed, type LettersUsedProps } from './components/LettersUsed'
 
 import { WORDS, type Challenge } from './utils/words'
 
 function App() {
+  const [score, setScore] = useState(0)
   const [attempts, setAttempts] = useState(0)
   // guardar a letra 
   const [letter, setLetter] = useState("")
   const [challenge, setChallenge] = useState<Challenge | null>(null)
 
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
+
+
   function handleRestartGame(){
     alert("reiniciar o jogo")
   }
+
+
+  function handleConfirm(){
+    if(!challenge){
+      return
+    }
+
+    if(!letter.trim()){
+      alert("Digite uma letra")
+      return
+    }
+
+    const value = letter.toUpperCase()
+
+    const exist = lettersUsed.find((used)=> used.value.toUpperCase() === value)
+
+    if(exist){
+      alert(`Você já usou a letra ${value}!`)
+      return
+    }
+
+    const hits = challenge.word.toUpperCase().split("").filter((char) => char === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState,{value, correct}])
+    setScore(currentScore)
+    setLetter("")
+    
+  }
+
   function startGame(){
     const index = Math.floor(Math.random()* WORDS.length)
 
@@ -30,6 +66,7 @@ function App() {
     setAttempts(0)
     setLetter("")
   }
+
 
   useEffect(() => {
     startGame()
@@ -43,7 +80,7 @@ function App() {
     <div className={style.container}>
       <main>
          <Header current={attempts} max={10} onRestart={handleRestartGame}/>
-         <Tip tip="Um framework JavaScript para construir interfaces de usuário" />
+         <Tip tip={challenge.tip} />
 
           <div className={style.word} >
 
@@ -57,11 +94,11 @@ function App() {
           <h4>Palpite</h4>
 
           <div className={style.guess}>
-            <Input autoFocus  maxLength={1} placeholder='?'/>
-            <Button title="Confirmar" />
+            <Input autoFocus value={letter} maxLength={1} placeholder='?' onChange={(e)=> setLetter(e.target.value)}/>
+            <Button title="Confirmar" onClick={handleConfirm} />
           </div>
 
-          <LettersUsed />
+          <LettersUsed data= {lettersUsed} />
       </main>
     </div>
   )
